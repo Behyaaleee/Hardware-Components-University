66e0bff2448f5abfe29cc8f14701051bdc0d02c4
Omar El-Bealy <64804703+Behyaaleee@users.noreply.github.com>
2023-07-04 00:26:26 +0300

    Add files via upload

---
+++ isim/sequencegen_sequencegen_sch_tb_isim_beh.exe.sim/work/sequencegen_sequencegen_sch_tb_isim_beh.exe_main.c
@@ -0,0 +1,55 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+#include "xsi.h"
+
+struct XSI_INFO xsi_info;
+
+char *IEEE_P_1242562249;
+char *IEEE_P_2592010699;
+char *STD_STANDARD;
+char *VL_P_2533777724;
+char *UNISIM_P_0947159679;
+
+
+int main(int argc, char **argv)
+{
+    xsi_init_design(argc, argv);
+    xsi_register_info(&xsi_info);
+
+    xsi_register_min_prec_unit(-12);
+    work_m_00000000004134447467_2073120511_init();
+    unisims_ver_m_00000000003510477262_0709700939_init();
+    unisims_ver_m_00000000000236260522_2449448540_init();
+    work_m_00000000004060778967_2491694526_init();
+    unisims_ver_m_00000000000924517765_3125220529_init();
+    work_m_00000000003847843200_3057990972_init();
+    ieee_p_2592010699_init();
+    ieee_p_1242562249_init();
+    unisim_p_0947159679_init();
+    vl_p_2533777724_init();
+    work_a_3621364728_3212880686_init();
+
+
+    xsi_register_tops("work_a_3621364728_3212880686");
+    xsi_register_tops("work_m_00000000004134447467_2073120511");
+
+    IEEE_P_1242562249 = xsi_get_engine_memory("ieee_p_1242562249");
+    IEEE_P_2592010699 = xsi_get_engine_memory("ieee_p_2592010699");
+    xsi_register_ieee_std_logic_1164(IEEE_P_2592010699);
+    STD_STANDARD = xsi_get_engine_memory("std_standard");
+    VL_P_2533777724 = xsi_get_engine_memory("vl_p_2533777724");
+    UNISIM_P_0947159679 = xsi_get_engine_memory("unisim_p_0947159679");
+
+    return xsi_run_simulation(argc, argv);
+
+}
